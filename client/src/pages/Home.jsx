bf330c8240ff9211d7d9a238d39a6581ff7c07fd
abbu solihin alhakim <alhakimabusolihin@gmail.com>
2025-07-04 22:10:59 +0700

    membuat home pt1

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,59 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+
+import state from "../store";
+
+import { CustomButton } from "../components";
+
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../config/motion";
+
+const Home = () => {
+  const snap = useSnapshot(state);
+
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                LET'S <br className="xl:block hidden" /> DO IT.{" "}
+              </h1>
+            </motion.div>
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md font-normal text-gray-600 text-base">
+                Buat baju yang eksklusif dan unik dengan fitur-fitur website 3D
+                kami. <strong>Mari wujudkan dan bangun imaginasi mu</strong> Dan
+                temukan style sejatimu Disini.
+              </p>
+
+              <CustomButton
+                type="filled"
+                title="Ayo Buat Sesukamu"
+                handleClick={() => (state.intro = false)}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Home;
