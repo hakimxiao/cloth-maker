5d9c2b1dacd307bb993b2432f5604d8078563076
abbu solihin alhakim <alhakimabusolihin@gmail.com>
2025-07-05 19:38:05 +0700

    Mengisi component customizer dan baru saja melakukkan pembuatan left menu bar pt1

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import state from "../store";
 import { useSnapshot } from "valtio";
 
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useSnapshot } from "valtio";
+
+import config from "../config/config";
+import state from "../store";
+import { download } from "../assets";
+import { downloadCanvasToImage } from "../config/helpers";
+import { DecalTypes, EditorTabs, FilterTabs } from "../config/constants";
+import { fadeAnimation, slideAnimation } from "../config/motion";
+
+import {
+  AIPicker,
+  ColorPicker,
+  CustomButton,
+  FilePicker,
+  Tab,
+} from "../components";
 
 const Customizer = () => {
-  return <div>Customizer</div>;
+  const snap = useSnapshot(state);
+
+  return (
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          <motion.div
+            key="Custom"
+            className="absolute top-0 left-0 z-10"
+            {...slideAnimation("left")}
+          >
+            <div className="flex items-center min-h-screen">
+              <div className="editortabs-container tabs">
+                {EditorTabs.map((tab) => (
+                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                ))}
+              </div>
+            </div>
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default Customizer;
