d43301690606011d53a29ae4978819a8f88cab9c
abbu solihin alhakim <alhakimabusolihin@gmail.com>
2025-07-15 23:59:01 +0700

    1. Melakukkan pendaftaran di platform.openai.com untuk mendapatkan API_KEY || 2. inisialisasi openai di dalam route dalle.routes.js || 3. membuat file .env berisikan apikey || 4. mengedit sedikit dibagian index.js

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -7,8 +7,33 @@ dotenv.config();
 
 const router = express.Router();
 
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+
 router.route("/").get((req, res) => {
     res.status(200).json({message: "Hello from DALL-E ROUTES"});
 });
+
+router.route("/").post(async(req, res) => {
+    try {
+        const {prompt} = req.body;
+
+        const response = await openai.images.generate({
+            prompt,
+            n: 1,   // banyak gambar
+            size: "1024x1024",
+            response_format: "b64_json"
+        });
+
+        const image = response.data[0].b64_json;
+
+        res.status(200).json({photo: image});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "Something went wrong"});
+    }
+})
 
 export default router;
